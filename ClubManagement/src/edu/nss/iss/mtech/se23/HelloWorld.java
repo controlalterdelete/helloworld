5ca7a2a21e5ef02261594b56bf3f75236c9f3293
controlalterdelete <a0135921@u.nus.edu>
2015-02-28 14:45:46 +0800

    Updated Hello world

--- ClubManagement/src/edu/nss/iss/mtech/se23/HelloWorld.java
+++ ClubManagement/src/edu/nss/iss/mtech/se23/HelloWorld.java
@@ -2,6 +2,6 @@ package edu.nss.iss.mtech.se23;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world, good morning");
 	}
 }
